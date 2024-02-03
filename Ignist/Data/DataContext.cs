e3891639972f37953adace1958b2e6c228c63df1
hamid <89603079+HamidHamrah@users.noreply.github.com>
2024-02-03 12:32:48 +0100

    Have implimented the authorization endpoints.

---
+++ Ignist/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Ignist.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Ignist.Data
+{
+    public class DataContext : IdentityDbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+		public DbSet<publications> publications { get; set; }
+	}
+}
+
---
+++ Ignist/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ignist.Models;
+using Ignist.Data;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:DataContextConnection"]);
+});
+
+//Implementing the autherzation here.
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<DataContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapIdentityApi<IdentityUser>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
