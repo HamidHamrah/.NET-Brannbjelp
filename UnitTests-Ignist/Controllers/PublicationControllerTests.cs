81a0a990d2a7865c5a06b72bdf5dfd1578edc661
HanneFjeldaas <hanne-lf@hotmail.com>
2024-03-15 12:42:15 +0100

    Implemented unit tests for updatePublication and addPublication.
    tests for updatePublication are not complete

--- UnitTests-Ignist/Controllers/PublicationControllerTests.cs
+++ UnitTests-Ignist/Controllers/PublicationControllerTests.cs
@@ -11,9 +11,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using Microsoft.Azure.Cosmos;
 
-//Kan teste: Post, Delete og GetAllPublications()
-//Update er ikke klar for testing 
-
 namespace UnitTests_Ignist.Controllers;
 
 public class PublicationControllerTests
@@ -25,7 +22,7 @@ public class PublicationControllerTests
         //Testen simulerer en liste med to publikasjoner i databasen. 
         //Tester at antall publikasjoner i listen er som forventet (2 stk i dette tilfellet).
         //Sjekker om verdien av OkObjectResult er av typen List<Publication> (forventet resultat av metoden)
-        //Sjekker at resultatet er av typen OK\kObjekctResult, som indikerer at forespørselen er vellykket.
+        //Sjekker at resultatet er av typen OkObjekctResult, som indikerer at forespørselen er vellykket.
         //Sjekker om listen som opprettes i "arrange" er lik listen som returneres fra metoden.  
 
         //arrange
@@ -70,9 +67,8 @@ public class PublicationControllerTests
     public async Task TestGetAllPublications_Negative()
     {
         //Denne testen sjekker hva som skjer når det ikke finnes noen publikasjoner.
-        //Sjekker at metoden returnerer en tom liste når det ikke er noen i
+        //Sjekker at metoden returnerer NotFound når det ikke er noen publikasjoner
         //databasen
-        //*
 
         //arrange
         var emptyPublicationList = new List<Publication>();
@@ -85,11 +81,112 @@ public class PublicationControllerTests
         var result = await publicationController.GetAllPublications();
 
         //assert
-        var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        var publications = Assert.IsType<List<Publication>>(okResult.Value);
-        Assert.Empty(publications);
+        Assert.IsType<NotFoundResult>(result.Result); 
     }
 
+    [Fact]
+    public async Task TestGetLastPublication_Positive()
+    {
+        //Denne testen sjekker at metoden returnerer riktig verdi (OkObjectResult)
+        //og at id'en som returneres er den forventede Id'en
+
+        //arrange
+        var newPublication = new Publication
+        {
+            Id = "1",
+            Title = "Tittel publikasjon",
+            Content = "plds duedn edjandks nf",
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now,
+            UserId = "user123",
+            ParentId = "parent123"
+        };
+
+        var mockPublicationRepository = new Mock<IPublicationsRepository>();
+        mockPublicationRepository.Setup(repo => repo.GetLastPublicationAsync()).
+            ReturnsAsync(newPublication);
+        var publicationController = new PublicationsController(mockPublicationRepository.Object);
+
+        //act
+        var result = await publicationController.GetLastPublication();
+
+        //assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.Equal(newPublication.Id, okResult.Value);
+    }
+
+    [Fact]
+    public async Task TestGetLastPublication_Negative()
+    {
+        //Testen sjekker at metoden returnerer NotFoundObject dersom det ikke er noen
+        //publikasjoner i mock-databasen. 
+
+        //arrange
+        var mockPublicationRepository = new Mock<IPublicationsRepository>();
+        mockPublicationRepository.Setup(repo => repo.GetLastPublicationAsync());
+        var publicationController = new PublicationsController(mockPublicationRepository.Object);
+
+        //act
+        var result = await publicationController.GetLastPublication();
+
+        //assert
+        var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+        Assert.Equal(404, notFoundObjectResult.StatusCode);
+    }
+
+    [Fact]
+    public async Task TestGetPublication_Positive()
+    {
+        //Testen sjekker at metoden returnerer korrekt Id 
+
+        //arrange
+        var newPublication = new Publication
+        {
+            Id = "1",
+            Title = "Tittel publikasjon",
+            Content = "plds duedn edjandks nf",
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now,
+            UserId = "user123",
+            ParentId = "parent123"
+        };
+
+        var mockPublicationRepository = new Mock<IPublicationsRepository>();
+        mockPublicationRepository.Setup(repo => repo.GetPublicationByIdAsync(newPublication.Id, newPublication.UserId)).ReturnsAsync(newPublication);
+        var publicationController = new PublicationsController(mockPublicationRepository.Object);
+
+        //act
+        var result = await publicationController.GetPublication(newPublication.Id, newPublication.UserId);
+
+        //assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var publication = Assert.IsType<Publication>(okResult.Value);
+        Assert.Equal(newPublication.Id, publication.Id);
+    }
+
+    [Fact]
+    public async Task TestGetPublication_Negative()
+    {
+        //Testen simulerer at vi leter etter userId og publicationId som ikke finnes i databasen
+        //Sjekker at metoden da returnerer feilmelding og Assert av typen 'NotFoundObjectResult'
+
+        //arrange
+        var userId = "user123wrong";
+        var publicationId = "wrong";
+        var mockPublicationRepository = new Mock<IPublicationsRepository>();
+        mockPublicationRepository.Setup(repo => repo.GetPublicationByIdAsync(publicationId, userId)).ReturnsAsync((Publication?)null);
+        var publicationController = new PublicationsController(mockPublicationRepository.Object);
+
+        //act
+        var result = await publicationController.GetPublication(publicationId, userId);
+
+        //assert
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+        var expectedMessage = "Publication not found.";
+        Assert.Equal(expectedMessage, notFoundResult.Value);
+    }
+
+    
     [Fact]
     public async Task TestDeletePublication_Positive()
     {
@@ -203,5 +300,110 @@ public class PublicationControllerTests
             publicationController.AddPublication(newPublication));
     }
 
- 
+    //fungerer ikke som den skal
+    [Fact]
+    public async Task TestUpdatePublication_Positive()
+    {
+        //Sjekker at update-metoden returnerer korrekt type respons og at UpdatePublicationAsync-metoden
+        //kalles med riktig publikasjon
+
+        //arrange
+        string id = "1";
+        var oldPublication = new Publication
+        {
+            Id = id,
+            Title = "Titteli",
+            Content = "Dette er innholdet i publikasjonen",
+            UserId = "user123",
+            ParentId = "parent123",
+        };
+        var newPublication = new Publication
+        {
+            Id = id,
+            Title = "Ny tittel",
+            Content = "Oppdaterer innholdet",
+            UserId = "user123",
+            ParentId = "parent123",
+        };
+
+        var mockPublicationRepository = new Mock<IPublicationsRepository>();
+        mockPublicationRepository.Setup(repo => repo.AddPublicationAsync(oldPublication));
+        var publicationController = new PublicationsController(mockPublicationRepository.Object);
+
+        //act
+        var result = await publicationController.UpdatePublication(id, newPublication);
+
+        //assert
+        mockPublicationRepository.Verify(repo => repo.UpdatePublicationAsync(newPublication), Times.Once);
+        Assert.IsType<ActionResult<Publication>>(result);
+        Assert.Equal(newPublication.Title, oldPublication.Title);
+        Assert.Equal(newPublication.Content, oldPublication.Content);
+        
+    }
+
+    [Fact]
+    public async Task TestUpdatePublication_InvalidId()
+    {
+        //Testen forsøker å oppdatere en publikasjon, men med feil id. 
+        //Sjekker at metoden returnerer feilmelding, returnerer assert av type
+        //'BadRequest', og at updatemetoden ikke gjennomføres.
+
+        //arrange
+        string id = "1";
+        var invalidId = "2"; 
+        var updatedPublication = new Publication
+        {
+            Id = invalidId,
+            Title = "Updated Title",
+            Content = "Updated Content",
+            UserId = "user123",
+            ParentId = "parent123",
+        };
+
+        var mockPublicationRepository = new Mock<IPublicationsRepository>();
+        var publicationController = new PublicationsController(mockPublicationRepository.Object);
+
+        //act
+        var result = await publicationController.UpdatePublication(id, updatedPublication);
+
+        //assert
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal("The ID of the publication does not match the request.", badRequestResult.Value);
+        mockPublicationRepository.Verify(repo => repo.UpdatePublicationAsync(updatedPublication), Times.Never);
+
+    }
+
+    [Fact]
+    public async Task UpdatePublication_PublicationNotFound()
+    {
+        //En test som sjekker tilfellet dersom publikasjonen ikke blir funnet i databasen
+        //Testen sjekker at metoden returnerer en CosmosException og NotFound status/respons
+
+        //arrange
+        string id = "1";
+        var updatedPublication = new Publication
+        {
+            Id = id,
+            Title = "Updated Title",
+            Content = "Updated Content",
+            UserId = "user123",
+            ParentId = "parent123",
+        };
+
+        var mockPublicationRepository = new Mock<IPublicationsRepository>();
+        mockPublicationRepository.Setup(repo => repo.UpdatePublicationAsync(updatedPublication))
+                      .ThrowsAsync(new CosmosException("Publication not found.", HttpStatusCode.NotFound, 404, "", 1.0));
+
+        var publicationController = new PublicationsController(mockPublicationRepository.Object);
+
+        //act
+        var result = await publicationController.UpdatePublication(id, updatedPublication);
+
+        //assert
+        Assert.IsType<NotFoundObjectResult>(result.Result);
+        mockPublicationRepository.Verify(repo => repo.UpdatePublicationAsync(updatedPublication), Times.Once);
+    }
+
+
+
 }
